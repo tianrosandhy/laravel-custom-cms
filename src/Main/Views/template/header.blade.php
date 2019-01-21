f0f367a14a19af302395d490ee236854008959bc
Christian <tianrosandhy@gmail.com>
2019-01-21 15:47:58 +0800

    language switcher

--- src/Main/Views/template/header.blade.php
+++ src/Main/Views/template/header.blade.php
@@ -5,7 +5,7 @@
       <ul class="nav navbar-nav">
         <li class="nav-item mobile-menu hidden-md-up float-xs-left"><a class="nav-link nav-menu-main menu-toggle hidden-xs"><i class="icon-menu5 font-large-1"></i></a></li>
         <li class="nav-item"><a href="index.html" class="navbar-brand nav-link">
-        	@include ('main::template.components.logo')
+          @include ('main::template.components.logo')
         </a></li>
         <li class="nav-item hidden-md-up float-xs-right"><a data-toggle="collapse" data-target="#navbar-mobile" class="nav-link open-navbar-container"><i class="icon-ellipsis pe-2x icon-icon-rotate-right-right"></i></a></li>
       </ul>
@@ -16,82 +16,38 @@
           <li class="nav-item hidden-sm-down"><a class="nav-link nav-menu-main menu-toggle hidden-xs"><i class="icon-menu5">         </i></a></li>
         </ul>
         <ul class="nav navbar-nav float-xs-right">
-          <li class="dropdown dropdown-language nav-item"><a id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle nav-link"><i class="flag-icon flag-icon-gb"></i><span class="selected-language">English</span></a>
+          <li class="dropdown dropdown-language nav-item"><a id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle nav-link">
+            @if(current_lang() == 'en')
+            <i class="flag-icon flag-icon-gb"></i>
+            @else
+            <i class="flag-icon flag-icon-id"></i>
+            @endif
+          </a>
             <div aria-labelledby="dropdown-flag" class="dropdown-menu">
-            	<a href="#" class="dropdown-item"><i class="flag-icon flag-icon-gb"></i> English</a>
-            	<a href="#" class="dropdown-item"><i class="flag-icon flag-icon-id"></i> Indonesian</a>
+              <a href="#" data-lang="en" class="dropdown-item btn-lang-switcher reload"><i class="flag-icon flag-icon-gb"></i> English</a>
+              <a href="#" data-lang="id" class="dropdown-item btn-lang-switcher reload "><i class="flag-icon flag-icon-id"></i> Indonesian</a>
           </li>
 
-		      @if(config('cms.admin.components.notification'))
-          <li class="dropdown dropdown-notification nav-item"><a href="#" data-toggle="dropdown" class="nav-link nav-link-label"><i class="ficon icon-bell4"></i><span class="tag tag-pill tag-default tag-danger tag-default tag-up">5</span></a>
-            <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-              <li class="dropdown-menu-header">
-                <h6 class="dropdown-header m-0"><span class="grey darken-2">Notifications</span><span class="notification-tag tag tag-default tag-danger float-xs-right m-0">5 New</span></h6>
-              </li>
-              <li class="list-group scrollable-container"><a href="javascript:void(0)" class="list-group-item">
-                  <div class="media">
-                    <div class="media-left valign-middle"><i class="icon-cart3 icon-bg-circle bg-cyan"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading">You have new order!</h6>
-                      <p class="notification-text font-small-3 text-muted">Lorem ipsum dolor sit amet, consectetuer elit.</p><small>
-                        <time datetime="2015-06-11T18:29:20+08:00" class="media-meta text-muted">30 minutes ago</time></small>
-                    </div>
-                  </div></a><a href="javascript:void(0)" class="list-group-item">
-                  <div class="media">
-                    <div class="media-left valign-middle"><i class="icon-monitor3 icon-bg-circle bg-red bg-darken-1"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading red darken-1">99% Server load</h6>
-                      <p class="notification-text font-small-3 text-muted">Aliquam tincidunt mauris eu risus.</p><small>
-                        <time datetime="2015-06-11T18:29:20+08:00" class="media-meta text-muted">Five hour ago</time></small>
-                    </div>
-                  </div></a><a href="javascript:void(0)" class="list-group-item">
-                  <div class="media">
-                    <div class="media-left valign-middle"><i class="icon-server2 icon-bg-circle bg-yellow bg-darken-3"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading yellow darken-3">Warning notifixation</h6>
-                      <p class="notification-text font-small-3 text-muted">Vestibulum auctor dapibus neque.</p><small>
-                        <time datetime="2015-06-11T18:29:20+08:00" class="media-meta text-muted">Today</time></small>
-                    </div>
-                  </div></a><a href="javascript:void(0)" class="list-group-item">
-                  <div class="media">
-                    <div class="media-left valign-middle"><i class="icon-check2 icon-bg-circle bg-green bg-accent-3"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Complete the task</h6><small>
-                        <time datetime="2015-06-11T18:29:20+08:00" class="media-meta text-muted">Last week</time></small>
-                    </div>
-                  </div></a><a href="javascript:void(0)" class="list-group-item">
-                  <div class="media">
-                    <div class="media-left valign-middle"><i class="icon-bar-graph-2 icon-bg-circle bg-teal"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Generate monthly report</h6><small>
-                        <time datetime="2015-06-11T18:29:20+08:00" class="media-meta text-muted">Last month</time></small>
-                    </div>
-                  </div></a></li>
-              <li class="dropdown-menu-footer"><a href="javascript:void(0)" class="dropdown-item text-muted text-xs-center">Read all notifications</a></li>
-            </ul>
-          </li>
-          @endif
 
-
-			    @if(config('cms.admin.components.userinfo'))
+          @if(config('cms.admin.components.userinfo'))
           <li class="dropdown dropdown-user nav-item">
-          	<a href="#" data-toggle="dropdown" class="dropdown-toggle nav-link dropdown-user-link">
-          		<span class="avatar avatar-online">
-          			<img src="{{ (strlen(admin_data('image')) == 0) ? admin_asset('img/default-user.png') : (ImageService::pathExists(admin_data('image')) ? Storage::url(ImageService::getName(admin_data('image'), 'cropped')) : admin_asset('img/default-user.png')) }}" alt="" data-src="{{ (strlen(admin_data('image')) == 0) ? admin_asset('img/default-user.png') : (ImageService::pathExists(admin_data('image')) ? Storage::url(ImageService::getName(admin_data('image'), 'cropped')) : admin_asset('img/default-user.png')) }}" width="32" height="32" alt="avatar">
-          			<i></i>
-          		</span>
-          		<span class="user-name">{{ admin_data('name') }}</span>
-          	</a>
+            <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-link dropdown-user-link">
+              <span class="avatar avatar-online">
+                <img src="{{ (strlen(admin_data('image')) == 0) ? admin_asset('img/default-user.png') : (ImageService::pathExists(admin_data('image')) ? Storage::url(ImageService::getName(admin_data('image'), 'cropped')) : admin_asset('img/default-user.png')) }}" alt="" data-src="{{ (strlen(admin_data('image')) == 0) ? admin_asset('img/default-user.png') : (ImageService::pathExists(admin_data('image')) ? Storage::url(ImageService::getName(admin_data('image'), 'cropped')) : admin_asset('img/default-user.png')) }}" width="32" height="32" alt="avatar">
+                <i></i>
+              </span>
+              <span class="user-name">{{ admin_data('name') }}</span>
+            </a>
             <div class="dropdown-menu dropdown-menu-right">
-            	<a href="{{ url('/') }}" target="_blank" class="dropdown-item">
-            		<i class="icon-head"></i> Go To Site
-            	</a>
-            	<a href="{{ admin_url('my-profile') }}" class="dropdown-item">
-            		<i class="icon-mail6"></i> My Profile
-            	</a>
+              <a href="{{ url('/') }}" target="_blank" class="dropdown-item">
+                <i class="icon-head"></i> Go To Site
+              </a>
+              <a href="{{ admin_url('my-profile') }}" class="dropdown-item">
+                <i class="icon-mail6"></i> My Profile
+              </a>
               <div class="dropdown-divider"></div>
               <a href="{{ admin_url('logout') }}" class="dropdown-item">
-              	<i class="icon-power3"></i> Logout
+                <i class="icon-power3"></i> Logout
               </a>
             </div>
           </li>
