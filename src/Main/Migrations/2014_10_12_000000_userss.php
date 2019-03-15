b6608cee5e05343156ff5dcf6b16f44ee5bb289d
Christian <tianrosandhy@gmail.com>
2019-03-15 13:05:30 +0800

    fix migration conflict bug

--- src/Main/Migrations/2014_10_12_000000_userss.php
+++ src/Main/Migrations/2014_10_12_000000_userss.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Users extends Migration
+class Userss extends Migration
 {
     /**
      * Run the migrations.
